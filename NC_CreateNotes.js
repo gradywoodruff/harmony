5dbc36d733b9b68c55fe473393101c533f895aea
gradywoodruff <jgradywoodruffiv@gmail.com>
2021-10-02 12:13:44 -0400

    Formatting

--- NC_CreateNotes.js
+++ NC_CreateNotes.js
@@ -1,100 +1,107 @@
-/* --------------------------------------------------------------------------------- 
+/* ---------------------------------------------------------------------------------
  * NC_CreateNotes.js
  *
  * Jason Schleifer / 21 November 2018
  * Latest Revision: 1.1 -  23 November 2018, 1:30 PM
  * License: GPL v3
- * 
+ *
  * Description:
  * -----------
  * Creates a Notes drawing layer.
  * Sets the mode to a nice note pencil.
- * 
+ *
  * Note:
  * -----
  * This only works if you have a Composite node at the top layer named "Composite"
  * Only tested with Harmony Premium.
- * 
+ *
  * Installation:
  * -------------
  * https://docs.toonboom.com/help/harmony-16/premium/scripting/import-script.html
- * 
- * 
+ *
+ *
  * Updates:
  * -----
  * 1.1 - Added visibility node
- * 
+ *
  */
 
-
 /**
- * 
+ *
  * @return {void}
  */
 function NC_CreateNotes() {
+  const NODE_WIDTH = 130
+  const NODE_HEIGHT = 50
 
-    const NODE_WIDTH = 130;
-    const NODE_HEIGHT = 50;
+  // The composite node.  Change this variable if you need to connect to a different composite
+  var compNode = "Top/Composite"
 
-    // The composite node.  Change this variable if you need to connect to a different composite
-    var compNode = "Top/Composite";
+  // get the position of the composite node so we can place our new node near it
 
-    // get the position of the composite node so we can place our new node near it
+  var xPos = node.coordX(compNode) - NODE_WIDTH
+  var yPos = node.coordY(compNode) - NODE_HEIGHT
 
-    var xPos = node.coordX(compNode) - NODE_WIDTH;
-    var yPos = node.coordY(compNode) - NODE_HEIGHT;
+  scene.beginUndoRedoAccum("New Note")
 
+  var noteName = "Note"
+  var elemName = "Note"
+  var columnType = "DRAWING"
 
-    scene.beginUndoRedoAccum("New Note")
+  // increment if node name exists
+  extra = 1
+  underscore = "_"
+  while (node.subNodeByName(node.root(), elemName) != "") {
+    elemName = noteName + underscore + extra
+    ++extra
+  }
 
-    var noteName = "Note";
-    var elemName = "Note";
-    var columnType = "DRAWING";
+  // Create the element and column.
+  var elemId = element.add(
+    elemName,
+    "BW",
+    scene.numberOfUnitsZ(),
+    "SCAN",
+    "TVG"
+  )
+  if (elemId != -1) {
+    column.add(elemName, "DRAWING")
+    column.setElementIdOfDrawing(elemName, elemId)
+  }
 
+  // Create node in the network.
+  var vnode = node.add(node.root(), elemName, "READ", xPos, yPos, 0)
 
-    // increment if node name exists
-    extra = 1;
-    underscore = "_";
-    while (node.subNodeByName(node.root(), elemName) != "") {
-        elemName = noteName + underscore + extra;
-        ++extra;
-    }
+  node.linkAttr(vnode, "DRAWING.ELEMENT", elemName)
 
-    // Create the element and column.
-    var elemId = element.add(elemName, "BW", scene.numberOfUnitsZ(), "SCAN", "TVG");
-    if (elemId != -1) {
-        column.add(elemName, "DRAWING");
-        column.setElementIdOfDrawing(elemName, elemId);
-    }
+  // Create a visibility node
+  var vis = node.add(
+    node.root(),
+    elemName + "_VIS",
+    "VISIBILITY",
+    xPos,
+    yPos + NODE_HEIGHT + 5,
+    0
+  )
+  // Turn off softrender
+  node.setTextAttr(vis, "SOFTRENDER", frame.current(), "N")
 
-    // Create node in the network.
-    var vnode = node.add(node.root(), elemName, "READ", xPos, yPos, 0);
+  // connect node to vis, and vis to the comp
+  var numPorts = node.numberOfInputPorts(compNode)
+  var compPort = 0
+  node.link(vnode, 0, vis, 0, false, true)
+  node.link(vis, 0, compNode, numPorts, false, true)
 
-    node.linkAttr(vnode, "DRAWING.ELEMENT", elemName);
+  // Now make sure the Note node is selected
+  selection.clearSelection()
+  selection.addDrawingColumnToSelection(elemName)
 
-    // Create a visibility node
-    var vis = node.add(node.root(), (elemName + "_VIS"), "VISIBILITY", xPos, (yPos + NODE_HEIGHT + 5), 0);
-    // Turn off softrender
-    node.setTextAttr(vis, "SOFTRENDER", frame.current(), "N");
+  // switch the current tool to the pencil tool.
+  Action.perform("onActionChoosePencilTool()", "Timeline")
 
-    // connect node to vis, and vis to the comp
-    var numPorts = node.numberOfInputPorts(compNode);
-    var compPort = 0;
-    node.link(vnode, 0, vis, 0, false, true);
-    node.link(vis, 0, compNode, numPorts, false, true);
+  // Color the node
+  var noteColor = new ColorRGBA(114, 157, 173, 20)
+  node.setColor(vnode, noteColor)
 
-
-    // Now make sure the Note node is selected
-    selection.clearSelection();
-    selection.addDrawingColumnToSelection(elemName);
-
-    // switch the current tool to the pencil tool.
-    Action.perform("onActionChoosePencilTool()", "Timeline");
-
-    // Color the node
-    var noteColor = new ColorRGBA(114, 157, 173, 20);
-    node.setColor(vnode, noteColor);
-
-    scene.endUndoRedoAccum();
-
+  scene.endUndoRedoAccum()
 }
--- NC_Rename.js
+++ NC_Rename.js
@@ -1,90 +1,87 @@
-/* --------------------------------------------------------------------------------- 
+/* ---------------------------------------------------------------------------------
  * NC_Rename.js
  *
  * Jason Schleifer / 26 October 2018
  * Latest Revision: v2.0 - 25 November 2018, 10:04 AM
  * License: GPL v3
- * 
+ *
  * Description:
  * -----------
  * Launches a window to help you quickly rename the selected nodes.
- * 
+ *
  * Usage:
  * ------
  * Select the node(s) you want to rename. Choose NC_Rename
- * 
+ *
  * Requirements:
  * -------------
  * NC_Utils.js
- * 
+ *
  * Updates:
  * --------
  * v2.0 - added use of NC_Utils.js
- * 
+ *
  * Installation:
  * -------------
  * https://docs.toonboom.com/help/harmony-16/premium/scripting/import-script.html
- * 
+ *
  * Acknowledgement:
  * ----------------
  * This script wouldn't have been possible without the help from eAthis
  * https://forums.toonboom.com/harmony/support-and-troubleshooting/how-set-focus-lineedit-qtscript
  */
 
-include("NC_Utils.js");
+include("NC_Utils.js")
 
 /**
- * 
+ *
  * @return {void}
  */
 function NC_Rename() {
-    var myUi = NC_CreateWidget()
-    var replaceLE = new QLineEdit();
-    var replaceLELabel = new QLabel();
-    replaceLELabel.text = "Replace:";
-    var submit = new QPushButton();
-    submit.text = "OK";
-    var cancel = new QPushButton();
-    cancel.text = "CANCEL";
+  var myUi = NC_CreateWidget()
+  var replaceLE = new QLineEdit()
+  var replaceLELabel = new QLabel()
+  replaceLELabel.text = "Replace:"
+  var submit = new QPushButton()
+  submit.text = "OK"
+  var cancel = new QPushButton()
+  cancel.text = "CANCEL"
 
-    myUi.gridLayout.addWidget(replaceLELabel, 1, 0);
-    myUi.gridLayout.addWidget(replaceLE, 1, 1);
-    myUi.gridLayout.addWidget(submit, 2, 0);
-    myUi.gridLayout.addWidget(cancel, 2, 1);
-    //myUi.setWindowFlags(Qt.FramelessWindowHint);
+  myUi.gridLayout.addWidget(replaceLELabel, 1, 0)
+  myUi.gridLayout.addWidget(replaceLE, 1, 1)
+  myUi.gridLayout.addWidget(submit, 2, 0)
+  myUi.gridLayout.addWidget(cancel, 2, 1)
+  //myUi.setWindowFlags(Qt.FramelessWindowHint);
 
-    myUi.show();
-    replaceLE.setFocus(true); // here is the line you need !
+  myUi.show()
+  replaceLE.setFocus(true) // here is the line you need !
 
-    var renameAll = function() {
-        var _newName = replaceLE.text;
-        var n = selection.numberOfNodesSelected();
+  var renameAll = function () {
+    var _newName = replaceLE.text
+    var n = selection.numberOfNodesSelected()
 
-        for (i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
+      var selNode = selection.selectedNode(i)
+      var nodeNamePath = selNode.split("/")
+      var nodeName = nodeNamePath[nodeNamePath.length - 1]
 
-            var selNode = selection.selectedNode(i);
-            var nodeNamePath = selNode.split("/");
-            var nodeName = nodeNamePath[nodeNamePath.length - 1];
+      var newNodeName = _newName
+      if (n > 1) {
+        newNodeName = newNodeName + "_" + (i + 1)
+      }
 
-            var newNodeName = _newName;
-            if (n > 1) {
-                newNodeName = newNodeName + "_" + (i + 1);
-            }
+      var columnId = node.linkedColumn(selNode, "DRAWING.ELEMENT")
+      var elementKey = column.getElementIdOfDrawing(columnId)
+      var newColumnName = newNodeName
 
-            var columnId = node.linkedColumn(selNode, "DRAWING.ELEMENT");
-            var elementKey = column.getElementIdOfDrawing(columnId);
-            var newColumnName = newNodeName;
-
-            node.rename(selNode, newNodeName);
-            column.rename(columnId, newNodeName);
-            element.renameById(elementKey, newNodeName);
-        }
-
-        myUi.close();
+      node.rename(selNode, newNodeName)
+      column.rename(columnId, newNodeName)
+      element.renameById(elementKey, newNodeName)
     }
 
-    submit.clicked.connect(myUi, renameAll);
-    cancel.clicked.connect(myUi, myUi.close);
+    myUi.close()
+  }
 
-
+  submit.clicked.connect(myUi, renameAll)
+  cancel.clicked.connect(myUi, myUi.close)
 }
